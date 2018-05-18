718662a5eba117665360c2212122e35ef00f34e4
tevers <tom.j.evers@gmail.com>
2018-05-18 20:34:39 +0200

    Api weer gerepareerd

--- api.py
+++ api.py
@@ -11,3 +11,23 @@ N_ISLANDS = 5
 
 def make_field_with_value(value):
     return [[value for _ in range(DIMENSIONS[Y])] for _ in range(DIMENSIONS[X])]
+
+
+PLAYERS = [1, 2]
+FIELDS = {}
+for P in PLAYERS:
+    FIELDS[P] = make_field_with_value(EMPTY), []
+
+
+def place_island(x, y, player):
+    if FIELDS[player][0][x][y] != EMPTY:
+        raise ("How can you pick an island here?")
+    else:
+        FIELDS[player][0][x][y] = ISLAND
+
+
+def list_coords(x, y, dir, length):
+    result = []
+    for i in range(length):
+        result.append((x + i * dir[X], y + i * dir[Y]))
+    return result
